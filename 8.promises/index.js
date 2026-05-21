34d3e624ed7a328e852aa34a1cfc0ff7d248a09d
Rajibul Molla <rajibulat@gmail.com>
2026-05-21 12:02:54 +0530

    Promises

---
+++ 8.promises/index.js
@@ -0,0 +1,28 @@
+
+// function delayFn(time){
+//     return new Promise((resolve)=> setTimeout(resolve, time))
+// }
+
+// console.log("promise lecture starts");
+// delayFn(2000).then(()=>{
+//     console.log("After 2 second promise resolved");
+// })
+// console.log("end");
+
+
+function devidefn(num1,num2){
+    return new Promise((resolve,reject)=>{
+        if(num2===0){
+            reject("Cant devide by 0");
+        }
+        else{
+            resolve(num1/num2);
+        }
+    })
+}
+
+devidefn(10,0).then(result=> {
+    console.log(result);
+}).catch(error=>{
+    console.log(error)
+})
